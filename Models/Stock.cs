76f5b1914525e4328c09c9345336dea9e794f48c
goodjonte <goodjonte@gmail.com>
2023-10-07 20:58:56 +1300

    Restrucutred stock for 2.0 improvements

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,126 +1,122 @@
 ﻿using PaperCalc.Data;
 using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PaperCalc.Models
 {
+
+    //----- Paper Stock Classes -----//
     public class Stock
     {
         [Key]
         public Guid Id { get; set; }
-        public string Company { get; set; } = default!;
-        [Display(Name = "Product")]
-        public string ProductName { get; set; } = default!;
+        public string Supplier { get; set; } = default!;
+        [Display(Name = "Stock Brand")]
+        public string StockBrand { get; set; } = default!;
         public int Weight { get; set; } = default!;
-        [Display(Name = "Coating")]
+        [Display(Name = "Stock Type")]
+        public StockType StockType { get; set; }
+        [Display(Name = "Coating Type")]
         public CoatType CoatType { get; set; }
     }
-    public enum CoatType
+    public class Sra3Stock : Stock
     {
-        Adhesive,
-        Coated,
-        Uncoated,
-        Synthetic,
-        Matt,
-        Satin,
-        Gloss
-    }
-
-    public class AspeosStock : Stock
-    {
-        public string Size { get; set; } = default!;
-        [Display(Name = "Pack Cost")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PackCost { get; set; }
-
-        [Display(Name = "Pack Cost + GST")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PackCostGstIncluded { get { return Math.Round(PackCost * 1.15, 2); } }
-
+        public string Size { get; set; } = default!; //Possabily unneccesary
         [Display(Name = "Sheets Per Pack")]
         public double SheetsPerPack { get; set; }
-
+        [Display(Name = "Price/1000")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double PricePer1000 { get; set; }
+        [Display(Name = "Pack Cost")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double PricePerPack { get { return Math.Round(PricePer1000 / (1000 / SheetsPerPack), 2); } }
         [Display(Name = "Sheet Cost")]
         [DisplayFormat(DataFormatString = "{0:c}")]
-        public double SheetCost { get { return Math.Round(PackCost / SheetsPerPack, 2); } }
+        public double SheetCost { get { return Math.Round(PricePerPack / SheetsPerPack, 2); } }
+        [Display(Name = "Height Of 100 Sheets")]
+        public double HeighOf100Sheets { get; set; }
     }
-
+    public class DocumentsStock : Stock
+    {
+        public string Size { get; set; } = default!;
+        [Display(Name = "Sheets Per Pack")]
+        public double SheetsPerPack { get; set; }
+        [Display(Name = "Price/1000")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double PricePer1000 { get; set; }
+        [Display(Name = "Pack Cost")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double PricePerPack { get { return Math.Round(PricePer1000 / (1000 / SheetsPerPack), 2); } }
+        [Display(Name = "Sheet Cost")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double SheetCost { get { return Math.Round(PricePerPack / SheetsPerPack, 2); } }
+        [Display(Name = "Height Of 100 Sheets")]
+        public double HeighOf100Sheets { get; set; }
+    }
     public class EpsonStock : Stock
     {
-        public int Width { get; set; }
-        [Display(Name = "Roll Cost")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double RollCost { get; set; }
-
-        [Display(Name = "Roll Cost (GST Incl)")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double RollCostGstIncluded { get { return Math.Round(RollCost * 1.15, 2); } }
-
-        [Display(Name = "Rolls Length (meters)")]
-        public double RollsLength { get; set; }
-
-        [Display(Name = "Price Per Meter")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PricePerMeter { get { return Math.Round(RollCost / RollsLength, 2); } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA0 { get { return Width >= 841 ? (PricePerMeter / 1000) * 1189 : null; } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA1 { get { return PriceA0 != null ? (PricePerMeter / 1000) * 594 : Width >= 594 ? (PricePerMeter / 1000) * 841 : null; } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA2 { get { return Width >= 420 ? (PricePerMeter / 1000) * 420 : null; } }
     }
 
-    public class FlatStock : Stock
+    //----- Other Items Stock Classes -----//
+
+
+    public class DocumentsBindingCoverStock
     {
+        [Key]
+        public Guid Id { get; set; }
+        public string Supplier { get; set; } = default!;
+        [Display(Name = "Stock Brand")]
+        public string StockBrand { get; set; } = default!;
+        [Display(Name = "Stock Type")]
+        public StockType StockType { get; set; }
         public string Size { get; set; } = default!;
-        [Display(Name = "Pack Cost")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PackCost { get; set; }
-
-        [Display(Name = "Pack Cost + GST")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PackCostGstIncluded { get { return Math.Round(PackCost * 1.15, 2); } }
-
         [Display(Name = "Sheets Per Pack")]
         public double SheetsPerPack { get; set; }
-
+        [Display(Name = "Pack Cost")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double PricePerPack { get; set; }
         [Display(Name = "Sheet Cost")]
         [DisplayFormat(DataFormatString = "{0:c}")]
-        public double SheetCost { get { return Math.Round(PackCost / SheetsPerPack, 2); } }
+        public double SheetCost { get { return Math.Round(PricePerPack / SheetsPerPack, 2); } }
+    }
+    public class BindingCoilsStock
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public BindingCoilType BindingCoilType { get; set; }
+        public string RingsRatio { get; set; } = default!;
+        public int SheetsHeld { get; set; }
+        public double CoilSize { get; set; }
+        public double CoilsPerPack { get; set; }
+        public double PricePerPack { get; set; }
+        public double PricePerCoil { get { return Math.Round(PricePerPack / CoilsPerPack, 2); } }
+        public string? Description { get { return $"{CoilSize}mm  {BindingCoilType} bound"; } }
     }
 
-    public class LaminationStock : Stock
+    //----- Enums for Stock -----//
+
+    public enum CoatType
     {
-        public int Width { get; set; }
-        [Display(Name = "Roll Cost")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double RollCost { get; set; }
-
-        [Display(Name = "Roll Cost (GST Incl)")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double RollCostGstIncluded { get { return Math.Round(RollCost * 1.15, 2); } }
-
-        [Display(Name = "Rolls Length (meters)")]
-        public double RollsLength { get; set; }
-
-        [Display(Name = "Price Per Meter")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double PricePerMeter { get { return Math.Round(RollCost / RollsLength, 2); } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA0 { get { return Width >= 841 ? (PricePerMeter / 1000) * 1189 : null; } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA1 { get { return PriceA0 != null ? (PricePerMeter / 1000) * 594 : Width >= 594 ? (PricePerMeter / 1000) * 841 : null; } }
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double? PriceA2 { get { return Width >= 420 ? (PricePerMeter / 1000) * 420 : null; } }
-
-        public static double GetLaminationCost(PaperCalcContext context, double width, double length)
-        {
-            double lamUsed = width > length ? (width + 10) * 2 : (length + 10) * 2;
-            LaminationStock? lamStock = context.LaminationStock.FirstOrDefault();
-            if(lamStock != null)
-            {
-                return Math.Round(((lamStock.PricePerMeter * 2)/1000) * lamUsed, 2);
-            }
-            return 0;
-        }
+        Coated,
+        Uncoated,
+        Matt,
+        Satin,
+        Gloss,
+    }
+    public enum StockType
+    {
+        Adhesive,
+        Synthetic,
+        Paper,
+        Card,
+        CardTextured,
+        CardRecycled,
+        Acetate,
+        Leather,
+    }
+    public enum BindingCoilType
+    {
+        Plastic,
+        Wire,
     }
 }
